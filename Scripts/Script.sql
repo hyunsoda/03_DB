5cf002cccedfb1bb4c62eff81adbcb2981b9740a
hyunsoda <so0144@naver.com>
2024-03-06 16:18:55 +0900

    fix

--- Scripts/Script.sql
+++ Scripts/Script.sql
@@ -97,9 +97,212 @@ WHERE DEPT_ID = 'D9';
 
 
 
+-- EMPLOYEE2 테이블에서 BONUS를 받지 않는 사원의 
+-- BONUS를 0.1로 변경
+
+UPDATE EMPLOYEE2 SET BONUS = 0.1 WHERE BONUS IS NULL; -- 15행 수정
+
+SELECT EMP_NAME, BONUS FROM EMPLOYEE2;
+
+
+-------------------------------------------------------------------
+
+
+-- * 조건절을 설정하지 않고 UPDATE 구문 실행 시 
+-- 모든 행의 컬럼값 변경 *
+
+SELECT * FROM DEPARTMENT2;
+
+UPDATE DEPARTMENT2 SET DEPT_TITLE = '기술연구팀';
+
+ROLLBACK;
+
+
+------------------------------------------------------------------
+
+-- * 여러 컬럼을 한 번에 수정할 시 콤마(,)로 컬럼을 구분하면 됨.
+-- D9 / 총무부 -> D0 / 전략기획팀으로 수정
+
+UPDATE DEPARTMENT2 SET 
+DEPT_ID = 'D0', 
+DEPT_TITLE = '전략기획팀' -- WHERE절 직전 컬럼 작성 콤마 X
+WHERE DEPT_ID = 'D9'
+AND DEPT_TITLE = '총무부';
+
+SELECT * FROM DEPARTMENT2;
+
+
+--------------------------------------------------------------------
+
+-- * UPDATE시에도 서브쿼리를 사용 가능
+
+-- [작성법]
+-- UPDATE 테이블명 SET
+-- 컬럼명 = (서브쿼리)
+
+
+-- EPLOYEE2 테이블에서 평소에 유재식을 부러워하던 방명수 사원의 급여와 보너스율을 
+-- 유재식 사원과 동일하게 변경해 주기로 했다..
+-- 이를 반영하는 UPDATE문을 작성해라!
+
+-- 유재식의 급여
+SELECT SALARY FROM EMPLOYEE2 WHERE EMP_NAME = '유재식';
+
+-- 유재식의 보너스율
+SELECT BONUS FROM EMPLOYEE2 WHERE EMP_NAME = '유재식';
+
+
+-- 방명수 급여, 보너스 급여
+UPDATE EMPLOYEE2 SET 
+SALARY = (SELECT SALARY FROM EMPLOYEE2 WHERE EMP_NAME = '유재식'), -- 유재식의 급여 --3400000
+BONUS = (SELECT BONUS FROM EMPLOYEE2 WHERE EMP_NAME = '유재식') -- 유재식의 보너스율 -- 0.2
+WHERE EMP_NAME = '방명수';
+
+
+SELECT EMP_NAME, SALARY, BONUS
+FROM EMPLOYEE2 
+WHERE EMP_NAME IN('유재식','방명수');
+
+
+-----------------------------------------------------------------------------------
+
+-- 3. MERGE (병합)
+
+-- 구조가 같은 두 개의 테이블을 하나로 합치는 기능
+-- 테이블에서 지정하는 조건의 값이 존재하면 UPDATE
+-- 없으면 INSERT 됨
+
+
+CREATE TABLE EMP_M01
+AS SELECT * FROM EMPLOYEE;
 
 
 
+CREATE TABLE EMP_M02
+AS SELECT * FROM EMPLOYEE 
+WHERE JOB_CODE = 'J4';
+
+SELECT * FROM EMP_M01;
+SELECT * FROM EMP_M02;
+
+INSERT INTO EMP_M02
+VALUES (999, '곽두원', '561016-1234567', 'kwak_dw@kh.or.kr', '01011112222','D9', 'J4','S1',9000000, 0.5 ,NULL, SYSDATE, NULL, DEFAULT);
+		
+SELECT * FROM EMP_M01; -- 23명
+SELECT * FROM EMP_M02; -- 5명 (기존 4 + 신규 1)
+
+UPDATE EMP_M02 SET SALARY = 0;
+
+MERGE INTO EMP_M01 USING EMP_M02 ON(EMP_M01.EMP_ID = EMP_M02.EMP_ID)
+WHEN MATCHED THEN
+UPDATE SET
+	EMP_M01.EMP_NAME = EMP_M02.EMP_NAME,
+	EMP_M01.EMP_NO = EMP_M02.EMP_NO,
+	EMP_M01.EMAIL = EMP_M02.EMAIL,
+	EMP_M01.PHONE = EMP_M02.PHONE,
+	EMP_M01.DEPT_CODE = EMP_M02.DEPT_CODE,
+	EMP_M01.JOB_CODE = EMP_M02.JOB_CODE,
+	EMP_M01.SAL_LEVEL = EMP_M02.SAL_LEVEL,
+	EMP_M01.SALARY = EMP_M02.SALARY,
+	EMP_M01.BONUS = EMP_M02.BONUS,
+	EMP_M01.MANAGER_ID = EMP_M02.MANAGER_ID,
+	EMP_M01.HIRE_DATE = EMP_M02.HIRE_DATE,
+	EMP_M01.ENT_DATE = EMP_M02.ENT_DATE,
+	EMP_M01.ENT_YN = EMP_M02.ENT_YN
+WHEN NOT MATCHED THEN
+INSERT VALUES(EMP_M02.EMP_ID, EMP_M02.EMP_NAME, EMP_M02.EMP_NO, EMP_M02.EMAIL, 
+	         EMP_M02.PHONE, EMP_M02.DEPT_CODE, EMP_M02.JOB_CODE, EMP_M02.SAL_LEVEL, EMP_M02.SALARY, EMP_M02.BONUS, EMP_M02.MANAGER_ID, EMP_M02.HIRE_DATE, 
+	         EMP_M02.ENT_DATE, EMP_M02.ENT_YN);
+-- M02를 M01로 합침
+-- 기존에 있는 건 바뀐 값만 UPDATE
+-- 없는 건 INSERT됨
+	        
+SELECT * FROM EMP_M01;
+
+
+-------------------------------------------------------------------------
+
+-- 4. DELETE
+-- 테이블의 행을 삭제하는 구문
+
+-- [작성법]
+-- DELETE FROM 테이블명 WHERE 조건설정
+-- 만약에 WHERE 조건을 설정하지 않으면 모든 행이 다 삭제됨
+
+COMMIT;
+
+SELECT * FROM EMPLOYEE2 
+WHERE EMP_NAME = '장채현';
+
+-- EMPLOYEE2 테이블에서 이름이 '장채현'인 사원 정보 삭제
+DELETE FROM EMPLOYEE2 WHERE EMP_NAME = '장채현';
+
+-- 삭제 확인
+SELECT * FROM EMPLOYEE2 
+WHERE EMP_NAME = '장채현'; --> 조회 결과 없음
+
+
+ROLLBACK; -- 마지막 커밋 시점까지 돌아감
+
+
+-- ROLLBACK확인
+SELECT * FROM EMPLOYEE2 
+WHERE EMP_NAME = '장채현'; --> 조회 결과 있음
+
+
+-- EMPLOYEE2 테이블 전체 삭제
+DELETE FROM EMPLOYEE2; --> WHERE절 미작성 시 전체 삭제
+--> 24행 삭제
+
+-- 삭제 확인
+SELECT * FROM EMPLOYEE2;
+
+ROLLBACK;
+
+
+-------------------------------------------------
+
+-- 5. TRUNCATE (DDL입니다 DML아닙니다...)
+-- DML --> DATA정의어
+-- 테이블의 전체 행을 삭제하는 DDL
+-- DELETE보다 수행속도 더 빠르다.
+-- ROLLBACK을 통해 복구할 수 없다.
+
+
+-- TRUNCATE 테스트용 테이블 생성
+CREATE TABLE EMPLOYEE3
+AS SELECT * FROM EMPLOYEE2;
+
+-- 생성확인
+SELECT * FROM EMPLOYEE3;
+
+-- DELETE로 모든 데이터 삭제
+DELETE FROM EMPLOYEE3;
+
+-- 삭제 확인
+SELECT * FROM EMPLOYEE3;
+
+ROLLBACK;
+
+-- 롤백 후 복구 확인
+SELECT * FROM EMPLOYEE3; -- 복구 완료
+
+
+-- TRUNCATE로 삭제
+TRUNCATE TABLE EMPLOYEE3;
+
+-- 삭제 확인
+SELECT * FROM EMPLOYEE3; 
+
+
+ROLLBACK;
+
+-- ROLLBACK 후 복구 확인
+SELECT * FROM EMPLOYEE3; -- 복구 완됨을 확인!
+
+
+-- DELETE : 휴지통 벌리기
+-- TRUNCATE : 완전 삭제
 
 
 
