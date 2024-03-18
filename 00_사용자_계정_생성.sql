7e796677b9bb9d1669aaa1c043f4a06b987a680e
hyunsoda <so0144@naver.com>
2024-03-18 16:32:30 +0900

    fix

--- 00_사용자_계정_생성.sql
+++ 00_사용자_계정_생성.sql
@@ -14,17 +14,17 @@ ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE;
 
 -- 사용자 계정 생성
 	-- 관리자 계정이기때문에 가능
-CREATE USER exam_ash IDENTIFIED BY exam_ash;
+CREATE USER board_project IDENTIFIED BY board1234;
 
 
 -- 사용자 계정에 권한 부여
-GRANT RESOURCE, CONNECT TO exam_ash;
+GRANT RESOURCE, CONNECT TO board_project;
 		-- RESOURCE : 데이터베이스 객체를 생성하거나 관리하는데 필요한 권한을 줌
 		-- 데이터베이스 객체 : table, view, sequence
 		-- CONNECT : 접속 권한
 
 -- 객체가 생성될 수 있는 공간 할당량 지정
-ALTER USER exam_ash DEFAULT TABLESPACE SYSTEM QUOTA UNLIMITED ON SYSTEM;
+ALTER USER board_project DEFAULT TABLESPACE SYSTEM QUOTA UNLIMITED ON SYSTEM;
 	-- 테이블 만드는데 공간 제한이 없다
 
 
